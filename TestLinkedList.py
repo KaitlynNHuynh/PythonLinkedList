db24aa8bee3a3b1e3102e35dc9eef715d9eaf810
Kaitlyn Huynh <kaitlynhuynh@kaitlyns-mbp.myfiosgateway.com>
2021-08-23 20:48:31 -0400

    initial commit

--- TestLinkedList.py
+++ TestLinkedList.py
@@ -16,26 +16,53 @@ class TestLLMethods(unittest.TestCase):
 
         self.assertEqual( result, '[ 4, 3, 2, 1, 0 ]')
 
-    #def test_contains(self):
-    #    self.assertTrue('FOO'.isupper())
-    #    self.assertFalse('Foo'.isupper())
+    def test_contains(self):
+        ll = LinkedList()
+        for i in range(5):
+            ll.ll_add(i)
+        result = ll.ll_contains(2)
 
-    #def test_remove_first(self):
-    #    s = 'hello world'
-    #    self.assertEqual(s.split(), ['hello', 'world'])
-    #    # check that s.split fails when the separator is not a string
-    #    with self.assertRaises(TypeError):
-    #        s.split(2)
-    #def test_remove_at(self):
-    #    self.assertTrue('FOO'.isupper())
-    #    self.assertFalse('Foo'.isupper())
+        self.assertEqual( result, True )
 
-    #def test_length(self):
-    #    s = 'hello world'
-    #    self.assertEqual(s.split(), ['hello', 'world'])
-        # check that s.split fails when the separator is not a string
-    #    with self.assertRaises(TypeError):
-    #        s.split(2)
+        result = ll.ll_contains(8)
+        self.assertEqual( result, False )
+
+    def test_remove_first(self):
+        ll = LinkedList()
+        for i in range(5):
+            ll.ll_add(i)
+        ll.ll_remove_first()
+        result = '[ '
+        ptr = ll.head
+        for i in range(ll.length - 1):
+            result =  str(result) + str(ptr.value) + ', '
+            ptr = ptr.next
+        result = str(result) + str(ptr.value) +' ]'
+
+        self.assertEqual( result, '[ 3, 2, 1, 0 ]')
+
+    def test_remove_at(self):
+        ll = LinkedList()
+        for i in range(5):
+            ll.ll_add(i)
+        ll.ll_remove_at(2)
+        result = '[ '
+        ptr = ll.head
+        for i in range(ll.length - 1):
+            result =  str(result) + str(ptr.value) + ', '
+            ptr = ptr.next
+        result = str(result) + str(ptr.value) +' ]'
+
+        self.assertEqual( result, '[ 4, 3, 1, 0 ]')
+
+
+    def test_length(self):
+        ll = LinkedList()
+        for i in range(5):
+            ll.ll_add(i)
+        result = ll.length
+
+        self.assertEqual( result, 5)
 
 if __name__ == '__main__':
     unittest.main()
